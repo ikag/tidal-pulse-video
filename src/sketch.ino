98ababbce39d156fd3d5a858169908189886e2dc
Damián Silvani <munshkr@gmail.com>
2016-02-03 20:11:03 -0300

    Draw a circle every second for 200ms

--- src/sketch.ino
+++ src/sketch.ino
@@ -4,33 +4,31 @@
 #define W 136
 #define H 96
 
+#define DEFAULT_DELTA 1000
+#define DEFAULT_DELAY 200
+
 TVout tv;
-unsigned char x,y;
-unsigned char originx = 5;
-unsigned char originy = 80;
-unsigned char plotx = originx;
-unsigned char ploty = 40;
-char s[32];
-unsigned int n = 0;
-int index = 0;
-int messageLen = 32;
-char message[] = "...OVERLAY TEXT AND GRAPHICS ON A VIDEO SIGNAL...OVERLAY TEXT AND GRAPHICS ON A VIDEO SIGNAL";
-char saveChar;
+
+unsigned long pulse_dur;
+unsigned long pulse_delta;
 
 void setup()  {
-    tv.begin(NTSC, W, H);
+    tv.begin(PAL, W, H);
     initOverlay();
+
     tv.select_font(font6x8);
     tv.fill(0);
-    drawGraph();
-    randomSeed(analogRead(0));
+
+    pulse_delta = DEFAULT_DELTA;
+    pulse_dur = DEFAULT_DELAY;
 }
 
 // Initialize ATMega registers for video overlay capability.
 // Must be called after tv.begin().
 void initOverlay() {
     TCCR1A = 0;
-    // Enable timer1.  ICES0 is set to 0 for falling edge detection on input capture pin.
+    // Enable timer1.  ICES0 is set to 0 for falling edge detection on input
+    // capture pin.
     TCCR1B = _BV(CS10);
 
     // Enable input capture interrupt
@@ -47,51 +45,8 @@ ISR(INT0_vect) {
 }
 
 void loop() {
-    saveChar = message[index+22];
-    message[index+22] = '\0';
-
-    for(int x=6;x>=0;x--) {
-        if (x<6) {
-            tv.delay_frame(1);
-        }
-        tv.print(x, 87, message+index);
-
-        for(byte y=87;y<96;y++) {
-            tv.draw_line(0, y, 5, y, 0);
-            tv.draw_line(128, y, 134, y, 0);
-        }
-    }
-    message[index+22] = saveChar;
-    index++;
-    if (index > 45) {
-        index = 0;
-    }
-
-    sprintf(s, "%dms", millis());
-    tv.print(0, 0, s);
-
-
-    if (plotx++ > 120) {
-        tv.fill(0);
-        drawGraph();
-        plotx = originx + 1;
-        return;
-    }
-    byte newploty = ploty + random(0, 7) - 3;
-    newploty = constrain(newploty, 15, originy);
-    tv.draw_line(plotx-1, ploty, plotx, newploty, 1);
-    ploty = newploty;
-}
-
-void drawGraph() {
-    tv.draw_line(originx, 15, originx, originy, 1);
-    tv.draw_line(originx, originy, 120, originy, 1);
-    for(byte y=originy;y>15;y -= 4) {
-        tv.set_pixel(originx-1, y, 1);
-        tv.set_pixel(originx-2, y, 1);
-    }
-    for(byte x=originx;x<120;x += 4) {
-        tv.set_pixel(x, originy+1, 1);
-        tv.set_pixel(x, originy+2, 1);
-    }
+    tv.draw_circle(tv.hres()/2, tv.vres()/2, tv.vres()/3, WHITE);
+    tv.delay(pulse_dur);
+    tv.clear_screen();
+    tv.delay(pulse_delta);
 }
