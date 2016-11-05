b02a473efe6e27dc98572f55240e6cec9ec07843
Damián Silvani <munshkr@gmail.com>
2016-11-05 13:05:41 -0300

    New version with more pots and buttons to control shape

---
+++ DESIGN.md
@@ -0,0 +1,40 @@
+# Input controls
+
+* Momentary push button (PB1)
+* Mini push button (PB2)
+* 6 potentiometers (POT*)
+* 4 switches (SW*)
+* 1 red led (LED1)
+* 1 green led (LED2)
+
+A possible usage would be:
+
+  * Push buttons
+    - PB1: Draw now
+    - PB2: Change shape
+  * Potentiomenters
+    - POT1: Position X
+    - POT2: Position Y
+    - POT3: Angle
+    - POT4: Size
+    - POT5: Tempo (master clock) / Step size (external clock)
+    - POT6: Pulse duration
+  * Switches:
+    - SW1: Manual draw
+    - SW2: Clock: Master / external
+    - SW3: Always on / Pulse
+    - SW4: Alternate drawing cycle
+  * Leds
+    - LED1: Power
+    - LED2: Drawing
+
+# Build
+
+## BOM
+
+* PB1: Momentary push button
+* PB2: Mini push button
+* POT*: 6 potentiometers B10K (linear)
+* SW*: 4 switches
+* LED1: red
+* LED2: green
--- src/sketch.ino
+++ src/sketch.ino
@@ -2,15 +2,30 @@
 #include <fontALL.h>
 #include <pollserial.h>
 
+//#define DEBUG
+
 #define W 128
 #define H 96
 
 #define DEFAULT_DELAY 75
 
+#define PB1_DRAW   3
+
+#define POT1_POSX  A0       // position X
+#define POT2_POSY  A1       // position Y
+#define POT3_ANGLE A2       // angle
+#define POT4_SIZE  A3       // size
+#define POT5_TEMPO A4       // tempo / step size
+#define POT6_PDUR  A5       // pulse duration
+
 TVout tv;
 pollserial pserial;
 
+bool tick, draw;
+byte x, y, size, shape;
+byte new_x, new_y, new_size, new_shape;
 unsigned long pulse_dur;
+unsigned long pulse_ctr;
 
 void setup()  {
     tv.begin(PAL, W, H);
@@ -18,12 +33,20 @@ void setup()  {
 
     initOverlay();
 
+    pinMode(PB1_DRAW, INPUT_PULLUP);
+
     tv.select_font(font6x8);
     tv.fill(0);
 
-    tv.draw_circle(tv.hres()/2, tv.vres()/2, tv.vres()/3, WHITE);
+    draw = false;
 
+    pulse_ctr = 0;
     pulse_dur = DEFAULT_DELAY;
+
+    // Set default parameters for shape
+    x = tv.hres() / 2;
+    y = tv.vres() / 2;
+    size = tv.vres() / 3;
 }
 
 // Initialize ATMega registers for video overlay capability.
@@ -48,33 +71,61 @@ ISR(INT0_vect) {
 }
 
 void loop() {
-  return;
-  
+#ifdef DEBUG
+    int sensorValue, value;
+
+    // Print pot values
+    for (int i = 0; i < 6; i++) {
+        sensorValue = analogRead(i);
+        value = map(sensorValue, 0, 1023, 0, 255);
+
+        pserial.print(value, DEC);
+        pserial.print(' ');
+    }
+    pserial.print(digitalRead(PB1_DRAW));
+    pserial.println(';');
+#endif
+
     // check if data has been sent from the computer:
     if (pserial.available()) {
-        int sensorValue = analogRead(A2);
-        pulse_dur = map(sensorValue, 0, 1023, 0, 1000);
-
-        if (pulse_dur == 0) return;
-
         // read the most recent byte (which will be from 0 to 255):
         int val = pserial.read();
-        if (!val) {
-            tv.draw_circle(tv.hres()/2, tv.vres()/2, tv.vres()/3, WHITE);
-        }
+        tick = !val;
+        if (tick) draw = true;
+    } else {
+        tick = false;
+    }
 
-        // dots
-        if (val == 0) {
-            tv.draw_rect(10, 10, 5, 5, WHITE);
-        } else if (val == 1) {
-            tv.draw_rect(W-20, 10, 5, 5, WHITE);
-        } else if (val == 2) {
-            tv.draw_rect(10, H-20, 5, 5, WHITE);
-        } else {
-            tv.draw_rect(W-20, H-20, 5, 5, WHITE);
-        }
+    if (draw) {
+        new_shape = (analogRead(POT3_ANGLE) < 512) ? 0 : 1;
+        new_size = map(analogRead(POT4_SIZE), 0, 1023, 0, H/2);
+        new_x = map(analogRead(POT1_POSX), 0, 1023, new_size, W-new_size);
+        new_y = map(analogRead(POT2_POSY), 0, 1023, new_size, H-new_size);
 
-        tv.delay(pulse_dur);
+        if (tick || new_shape != shape || new_size != size || new_x != x || new_y != y) {
+            shape = new_shape;
+            size = new_size;
+            x = new_x;
+            y = new_y;
+
+            tv.clear_screen();
+
+            switch (shape) {
+            case 0:
+                tv.draw_circle(x, y, size, WHITE);
+                break;
+            case 1:
+                tv.draw_rect(x-size, y-size, size*2, size*2, WHITE);
+                break;
+            }
+        }
+    }
+
+    pulse_ctr++;
+    if (pulse_ctr > pulse_dur) {
+        pulse_dur = map(analogRead(POT6_PDUR), 0, 1023, 0, 1000);
+        pulse_ctr = 0;
+        draw = false;
         tv.clear_screen();
     }
 }
